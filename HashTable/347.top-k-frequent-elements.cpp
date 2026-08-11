a1bfeef343fdc9562c06b411122889510c6cb767
Freezecoke <gtopopjason@gmail.com>
2026-08-11 23:56:00 +0200

    Solve 347.Top K Frequent Elements: Unordered_map

---
+++ HashTable/347.top-k-frequent-elements.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=347 lang=cpp
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap;
+        for(int num : nums){
+            freqMap[num]++;
+        }
+
+        // Convert freqMap to freqVec
+        vector<pair<int, int>> freqVec;
+        for(auto& [num, count] : freqMap){
+            freqVec.push_back({num, count});
+        }
+
+        // Sort by frequency
+        sort(freqVec.begin(), freqVec.end(), [](const pair<int, int>& a, const pair<int, int>& b){
+            return a.second > b.second;
+        });
+        
+        vector<int> out;
+        for(int i=0; i<k; i++){
+            out.push_back(freqVec[i].first);
+        }
+        return out;
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 1, 1, 2, 2, 3};
+    int k = 2;
+    vector<int> result = solution.topKFrequent(nums, k);
+    cout << "Top " << k << " frequent elements: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
