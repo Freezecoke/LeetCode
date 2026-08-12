940ea6f7b49de9dd2d24958ae30f1f43895105ee
Freezecoke <gtopopjason@gmail.com>
2026-08-12 23:49:51 +0200

    Solve 347.Top K Frequent Elements: Unordered_map + bucket sort

--- HashTable/347.top-k-frequent-elements.cpp
+++ HashTable/347.top-k-frequent-elements.cpp
@@ -11,41 +11,81 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freqMap;
-        for(int num : nums){
-            freqMap[num]++;
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+
+        // Step 1: Count frequency
+        // 1 → 3
+        // 2 → 2
+        // 3 → 1
+        unordered_map<int, int> freq;
+
+        for (int num : nums)
+        {
+            freq[num]++;
         }
 
-        // Convert freqMap to freqVec
-        vector<pair<int, int>> freqVec;
-        for(auto& [num, count] : freqMap){
-            freqVec.push_back({num, count});
+        // Step 2: Create buckets
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        // frequency : num
+        // bucket[1] = [3]
+        // bucket[2] = [2]
+        // bucket[3] = [1]
+        for (auto &[num, count] : freq)
+        {
+            bucket[count].push_back(num);
         }
 
-        // Sort by frequency
-        sort(freqVec.begin(), freqVec.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-            return a.second > b.second;
-        });
-        
-        vector<int> out;
-        for(int i=0; i<k; i++){
-            out.push_back(freqVec[i].first);
+        vector<int> result;
+        for (int i = nums.size(); i >= 1 && result.size() < k; i--)
+        {
+            for (int num : bucket[i]){
+                result.push_back(num);
+            }
         }
-        return out;
+
+        return result;
     }
+
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int, int> freqMap;
+    //     for(int num : nums){
+    //         freqMap[num]++;
+    //     }
+
+    //     // Convert freqMap to freqVec
+    //     vector<pair<int, int>> freqVec;
+    //     for(auto& [num, count] : freqMap){
+    //         freqVec.push_back({num, count});
+    //     }
+
+    //     // Sort by frequency
+    //     sort(freqVec.begin(), freqVec.end(), [](const pair<int, int>& a, const pair<int, int>& b){
+    //         return a.second > b.second;
+    //     });
+
+    //     vector<int> out;
+    //     for(int i=0; i<k; i++){
+    //         out.push_back(freqVec[i].first);
+    //     }
+    //     return out;
+    // }
 };
 // @lc code=end
 
-int main() {
+int main()
+{
     Solution solution;
     vector<int> nums = {1, 1, 1, 2, 2, 3};
     int k = 2;
     vector<int> result = solution.topKFrequent(nums, k);
     cout << "Top " << k << " frequent elements: ";
-    for (int num : result) {
+    for (int num : result)
+    {
         cout << num << " ";
     }
     cout << endl;
