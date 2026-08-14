b5f4ea37d756b9f3daeddfdb40699cb88865e78e
Freezecoke <gtopopjason@gmail.com>
2026-08-15 00:09:34 +0200

    Review 20.valid-parentheses: Stack

--- Stack/20.valid-parentheses.cpp
+++ Stack/20.valid-parentheses.cpp
@@ -3,62 +3,72 @@
  *
  * [20] Valid Parentheses
  */
+
+// @lc code=start
 #include <string>
 #include <stack>
 #include <iostream>
 using namespace std;
-
-// @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        std::stack<char> stack;
-        for (char c : s){
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.empty()){
-                 return false;
-                }
-                
-                char top = stack.top();
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
+    bool isValid(string s)
+    {
+        stack<char> container;
+        for (char c : s)
+        {
+            if ((c == '(') || (c == '[') || (c == '{'))
+            {
+                container.push(c);
+            }
+            else
+            {
+                if (container.empty())
+                {
                     return false;
                 }
-                stack.pop();
+                if (c == ')' && container.top() != '(')
+                {
+                    return false;
+                }
+                if (c == ']' && container.top() != '[')
+                {
+                    return false;
+                }
+                if (c == '}' && container.top() != '{')
+                {
+                    return false;
+                }
+
+                container.pop();
             }
         }
-        return stack.empty();
+        return container.empty();
     }
 };
 // @lc code=end
 
-int main() {
+int main()
+{
     Solution solution;
-    // Test1
     {
-        std::cout << "Test1" << std::endl;
-        string input = "()[]";
-        bool result = solution.isValid(input);
-        std::cout << result << std::endl;
+        string s = "()[]{}";
+        bool result = solution.isValid(s);
+        cout << result << endl;
     }
-    // Test2
     {
-        std::cout << "Test2" << std::endl;
-        string input = "([])";
-        bool result = solution.isValid(input);
-        std::cout << result << std::endl;
+        string s = "([)]";
+        bool result = solution.isValid(s);
+        cout << result << endl;
     }
-
-    // Test3
     {
-        std::cout << "Test3" << std::endl;
-        string input = "([)]";
-        bool result = solution.isValid(input);
-        std::cout << result << std::endl;
+        string s = "]";
+        bool result = solution.isValid(s);
+        cout << result << endl;
     }
-
-    return 0;
+    {
+        string s = "(]";
+        bool result = solution.isValid(s);
+        cout << result << endl;
+    }
 }
